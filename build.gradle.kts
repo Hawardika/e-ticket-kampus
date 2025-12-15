3d6899eef893166afea289af85ebf0ca3ad7d364
Hawardika <224443026@mhs.polman-bandung.ac.id>
2025-12-15 17:08:03 +0700

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,31 +6,46 @@ plugins {
 
 group = "com.polman.oop.diagram2code"
 version = "1.0-SNAPSHOT"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("com.midtrans:java-library:3.0.0")
-    implementation("io.ktor:ktor-server-core-jvm:3.0.0")
-    implementation("io.ktor:ktor-server-netty-jvm:3.0.0")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.0.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.0.0")
-    implementation("io.ktor:ktor-server-status-pages-jvm:3.0.0")
-    implementation("io.ktor:ktor-server-cors-jvm:3.0.0")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.52.0")
+
+    // Ktor Server
+    implementation("io.ktor:ktor-server-core-jvm:3.3.3")
+    implementation("io.ktor:ktor-server-netty-jvm:3.3.3")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:3.3.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.3.3")
+    implementation("io.ktor:ktor-server-status-pages-jvm:3.3.3")
+    implementation("io.ktor:ktor-server-cors-jvm:3.3.3")
+
+    // Ktor HTTP Client (UNTUK MIDTRANS)
+    implementation("io.ktor:ktor-client-core-jvm:3.3.3")
+    implementation("io.ktor:ktor-client-cio-jvm:3.3.3")
+    implementation("io.ktor:ktor-client-content-negotiation-jvm:3.3.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:3.3.3")
+
+    // Database
     implementation("org.jetbrains.exposed:exposed-core:0.52.0")
     implementation("org.jetbrains.exposed:exposed-dao:0.52.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.52.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.52.0")
     implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("com.mysql:mysql-connector-j:8.4.0")
+
+    // Logging (AMAN)
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+
+    // Testing
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-server-test-host-jvm:3.0.0")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:3.3.3")
 }
 
-application { mainClass.set("app.ApplicationKt") }
+application {
+    mainClass.set("app.ApplicationKt")
+}
 
 tasks.test {
     useJUnitPlatform()
--- src/main/kotlin/presentation/routes/OrderRoutes.kt
+++ src/main/kotlin/presentation/routes/OrderRoutes.kt
@@ -1,7 +1,6 @@
 package app.presentation.routes
 
 import io.ktor.server.routing.*
-import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import app.domain.services.OrderService
